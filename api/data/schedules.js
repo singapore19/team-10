5f73dcfbd6117f12017321cff6d4ab28c4372d70
Han Qing <zhaohanqing96@gmail.com>
2019-10-04 12:50:35 +0800

    Added data

---
+++ api/data/schedules.js
@@ -0,0 +1,43 @@
+module.exports = {
+    schedules: [{
+        "date_of_schedule": "12/09/2019",
+      "driver_id": 456,
+      "driver_name": "Lin Siew Peng",
+      "driver_contact_no": "91257302",
+      "scheduled_requests": [
+            {
+                "request_id": 123,
+                "request_date": "12/09/2019",
+                "request_type": "Staff Transport",
+                "preferred_timeslot": "Noon",
+                "location_from": "70 Barker Rd, #05-01, Singapore 309936",
+                "location_to": "25 Geylang East Ave 1, #01-05, Singapore 381125",
+                "request_status": "Completed",
+                "telegram_chat_id": "rogerlee12",
+                "emergency_contact_no": "92130612"
+            },
+            {
+                "request_id": 215,
+                "request_date": "12/09/2019",
+                "request_type": "Staff Transport",
+                "preferred_timeslot": "Noon",
+                "location_from": "70 Barker Rd, #05-01, Singapore 309936",
+                "location_to": "25 Geylang East Ave 1, #01-05, Singapore 381125",
+                "request_status": "Completed",
+                "telegram_chat_id": "rogerlee12",
+                "emergency_contact_no": "92130612"
+            },  
+            {
+                "request_id": 263,
+                "request_date": "12/09/2019",
+                "request_type": "Delivery Of Donation",
+                "preferred_timeslot": "",
+                "location_from": "25 Geylang East Ave 1, #01-05, Singapore 381125",
+                "location_to": "70 Barker Rd, #05-01, Singapore 309936",
+                "request_status": "Completed",
+                "telegram_chat_id": "rogerlee12",
+                "emergency_contact_no": "92130612"
+            },        
+        ]
+    }]
+}
